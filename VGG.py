1e644ea1da4bebe20ae297f07e76f9e39b5ad653
Bossun Wang <vvmodouco@gmail.com>
2021-10-10 21:10:23 +0800

    cluster by new VGG model and splited two class without outlier which the whole shape as concentric circles

---
+++ VGG.py
@@ -0,0 +1,108 @@
+import torch
+from torch import optim, nn
+from torch.nn import functional as F
+from torchvision import models, transforms
+from torchvision.transforms import functional as TF
+from functools import partial
+
+
+class Scale(nn.Module):
+    def __init__(self, module, scale):
+        super().__init__()
+        self.module = module
+        self.register_buffer('scale', torch.tensor(scale))
+
+    def extra_repr(self):
+        return f'(scale): {self.scale.item():g}'
+
+    def forward(self, *args, **kwargs):
+        return self.module(*args, **kwargs) * self.scale
+
+
+class VGGFeatures(nn.Module):
+    poolings = {'max': nn.MaxPool2d, 'average': nn.AvgPool2d, 'l2': partial(nn.LPPool2d, 2)}
+    pooling_scales = {'max': 1., 'average': 2., 'l2': 0.78}
+
+    def __init__(self, layers, pooling='max'):
+        super().__init__()
+        self.layers = sorted(set(layers))
+
+        # The PyTorch pre-trained VGG-19 expects sRGB inputs in the range [0, 1] which are then
+        # normalized according to this transform, unlike Simonyan et al.'s original model.
+        # self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+        #                                       std=[0.229, 0.224, 0.225])
+
+        # The PyTorch pre-trained VGG-19 has different parameters from Simonyan et al.'s original
+        # model.
+        self.model = models.vgg19(pretrained=True).features[:self.layers[-1] + 1]
+        self.devices = [torch.device('cpu')] * len(self.model)
+
+        # Reduces edge artifacts.
+        self.model[0] = self._change_padding_mode(self.model[0], 'replicate')
+
+        pool_scale = self.pooling_scales[pooling]
+        for i, layer in enumerate(self.model):
+            if pooling != 'max' and isinstance(layer, nn.MaxPool2d):
+                # Changing the pooling type from max results in the scale of activations
+                # changing, so rescale them. Gatys et al. (2015) do not do this.
+                self.model[i] = Scale(self.poolings[pooling](2), pool_scale)
+
+        self.model.eval()
+        self.model.requires_grad_(False)
+
+    @staticmethod
+    def _change_padding_mode(conv, padding_mode):
+        new_conv = nn.Conv2d(conv.in_channels, conv.out_channels, conv.kernel_size,
+                             stride=conv.stride, padding=conv.padding,
+                             padding_mode=padding_mode)
+        with torch.no_grad():
+            new_conv.weight.copy_(conv.weight)
+            new_conv.bias.copy_(conv.bias)
+        return new_conv
+
+    @staticmethod
+    def _get_min_size(layers):
+        last_layer = max(layers)
+        min_size = 1
+        for layer in [4, 9, 18, 27, 36]:
+            if last_layer < layer:
+                break
+            min_size *= 2
+        return min_size
+
+    def distribute_layers(self, devices):
+        for i, layer in enumerate(self.model):
+            if i in devices:
+                device = devices[i]
+            self.model[i] = layer.to(device)
+            self.devices[i] = device
+
+    def forward(self, input, layers=None):
+        layers = self.layers if layers is None else sorted(set(layers))
+        h, w = input.shape[2:4]
+        min_size = self._get_min_size(layers)
+        if min(h, w) < min_size:
+            raise ValueError(f'Input is {h}x{w} but must be at least {min_size}x{min_size}')
+        feats = {'input': input}
+        # input = self.normalize(input)
+        for i in range(max(layers) + 1):
+            input = self.model[i](input.to(self.devices[i]))
+            if i in layers:
+                feats[i] = input
+        return feats
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device_plan = {0: device}
+    pooling = 'average'
+    style_layers = [1, 6, 11, 20, 29]
+    model = VGGFeatures(style_layers, pooling=pooling)
+    model.distribute_layers(device_plan)
+
+    batch_size = 4
+    input = torch.rand(batch_size, 3, 256, 256).to(device)
+    features = model(input, style_layers)
+
+    for style_layer in style_layers:
+        print(features[style_layer].size())
--- cluster.py
+++ cluster.py
@@ -1,96 +1,187 @@
 import torch
-from torchvision import datasets, transforms
 import os
 import numpy as np
 import umap
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import cv2
-from model import VGG19
 from sklearn.cluster import KMeans
 from shutil import copyfile
+from VGG import VGGFeatures
+import pickle
+from sklearn.cluster import SpectralClustering, AgglomerativeClustering, DBSCAN
 
 
-def load_image(image_path, rw, rh):
+def load_image(image_path, rw, rh, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     img = cv2.imread(image_path).astype(np.float32)
+    img = cv2.resize(img, (rw, rh))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    img = cv2.resize(img, (rw, rh))
-
-    img = torch.from_numpy(img)
-    img = img / 127.5 - 1.0
+    img = (img / 255.0 - mean) / std
+    img = img.transpose((2, 0, 1))
+    img = torch.from_numpy(img.astype(np.float32))
     return img
 
 
-def main():
-    batch_size = 64
-    data_dir = '../style_dataset'
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def extract_feature(data_dir):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device_plan = {0: device}
+    pooling = 'average'
+    style_layers = [1, 6, 11, 20, 29]
+    model = VGGFeatures(style_layers, pooling=pooling)
+    model.distribute_layers(device_plan)
 
-    vgg_model = 'vgg19-dcbb9e9d.pth'
-    VGG = VGG19(init_weights=vgg_model, feature_mode=True).to(device)
-    for param in VGG.parameters():
-        param.require_grad = False
-
-    train_features = torch.tensor([]).cpu()
+    data_size_list = []
     features = torch.tensor([]).cpu()
     image_path_list = []
 
-    # with torch.no_grad():
-    #     for dirPath, dirNames, fileNames in os.walk(data_dir):
-    #         for i, f in tqdm(enumerate(fileNames)):
-    #             if 'smooth' in dirPath:
-    #                 img_path = os.path.join(dirPath, f)
-    #                 image = load_image(img_path, 128, 128)
-    #                 input = image.permute(2, 0, 1).unsqueeze(0).to(device)
-    #                 feature = VGG(input)
-    #                 feature = feature.cpu()
-    #                 if i < 500:
-    #                     train_features = torch.cat((train_features, feature.view(1, -1)), 0)
-    #                 features = torch.cat((features, feature.view(1, -1)), 0)
-    #                 image_path_list.append(img_path)
-    #
-    #     features = features.cpu().detach().numpy()
-    #     np.save('train_features.npy', train_features)
-    #     np.save('features.npy', features)
-    #     np.save('image_path_list.npy', image_path_list)
+    with torch.no_grad():
+        for dirPath, dirNames, fileNames in os.walk(data_dir):
+            if 'smooth' == dirPath.split('/')[-1]:
+                data_size_list.append(len(fileNames))
+                for i, f in tqdm(enumerate(fileNames)):
+                    img_path = os.path.join(dirPath, f)
+                    image = load_image(img_path, 256, 256)
+                    input = image.unsqueeze(0).to(device)
+                    feature_dict = model(input)
+                    feature = feature_dict[29].cpu()
+                    features = torch.cat((features, feature.view(1, -1)), 0)
+                    image_path_list.append(img_path)
 
-    train_features = np.load('train_features.npy')
+        features = features.cpu().detach().numpy()
+        np.save('features.npy', features)
+        with open('image_path_list.pickle', 'wb') as f:
+            pickle.dump(image_path_list, f)
+        np.save('data_size_list.npy', np.array(data_size_list))
+
+
+def UMAP_embedding():
+    data_size_list = np.load('data_size_list.npy')
     features = np.load('features.npy')
-    # print(features.shape)
-    # print(image_path_list)
+    print('features size:', features.shape)
 
+    train_features = []
+    start = 0
+    end = 0
+    for data_size in data_size_list:
+        end += data_size
+        part_features = features[start:end]
+        part_index = np.random.choice(data_size, 1000)
+        train_features.extend(part_features[part_index])
+        start += data_size
+
+    train_features = np.array(train_features)
     reducer = umap.UMAP()
-    embedding = reducer.fit(train_features)
+    mapper = reducer.fit(train_features)
 
-    # plt.scatter(
-    #     embedding[:, 0],
-    #     embedding[:, 1])
-    # plt.gca().set_aspect('equal', 'datalim')
-    # plt.title('UMAP projection of the Anime dataset', fontsize=24)
-    # plt.show()
+    # Training with Labels and Embedding Unlabelled Test Data (Metric Learning with UMAP)
+    embedding = mapper.transform(features)
+    np.save('embedding.npy', embedding)
 
-    embedding = reducer.transform(features)
-    class_number = 4
-    kmeans = KMeans(n_clusters=class_number, random_state=0).fit(embedding)
+    plt.scatter(*embedding.T, s=1.0, cmap='Spectral', alpha=1.0)
+    plt.title('UMAP projection of the Anime dataset', fontsize=24)
+    plt.savefig('UMAP_metric_learning.jpg')
 
+
+def cluster(data_dir):
+    embedding = np.load('embedding.npy')
+    with open('image_path_list.pickle', 'rb') as f:
+        image_path_list = pickle.load(f)
+        image_path_list = np.array(image_path_list)
+
+    # exclude outlier
+    class_number = 2
+    clustering = SpectralClustering(n_clusters=class_number
+                                    , assign_labels='discretize'
+                                    , affinity='nearest_neighbors'
+                                    , random_state=0).fit(embedding)
+    fig, ax = plt.subplots(1, figsize=(14, 10))
     plt.scatter(
         embedding[:, 0],
         embedding[:, 1],
-        c=list(kmeans.labels_))
-    plt.gca().set_aspect('equal', 'datalim')
+        s=3.0, cmap='Spectral', alpha=1.0,
+        c=list(clustering.labels_))
+    plt.setp(ax, xticks=[], yticks=[])
+    cbar = plt.colorbar(boundaries=np.arange(class_number + 1) - 0.5)
+    cbar.set_ticks(np.arange(class_number))
     plt.title('UMAP projection of the Anime dataset', fontsize=24)
-    plt.savefig('UMAP_Kmeans_clusters.jpg')
+    plt.savefig('UMAP_SpectralClustering.jpg')
 
-    image_path_list = np.load('image_path_list.npy')
+    # cluster again exclude center cluster
+    second_embedding = embedding[clustering.labels_ == 0]
+    second_clustering = DBSCAN(eps=0.5, min_samples=2).fit(second_embedding)
+    # print(np.max(second_clustering.labels_))
+    # print(np.min(second_clustering.labels_))
+    plt.cla()
+    fig, ax = plt.subplots(1, figsize=(14, 10))
+    plt.scatter(
+        second_embedding[:, 0],
+        second_embedding[:, 1],
+        s=3.0, cmap='Spectral', alpha=1.0,
+        c=list(second_clustering.labels_))
+    plt.setp(ax, xticks=[], yticks=[])
+    cbar = plt.colorbar(boundaries=np.arange(np.min(second_clustering.labels_), np.max(second_clustering.labels_) + 1) - 0.5)
+    cbar.set_ticks(np.arange(np.min(second_clustering.labels_), np.max(second_clustering.labels_)))
+    plt.title('UMAP projection of the Anime dataset', fontsize=24)
+    plt.savefig('UMAP_DBSCAN.jpg')
+
+    max_cluster_index = 0
+    max_cluster_size = 0
+    for index in range(np.min(second_clustering.labels_), np.max(second_clustering.labels_)):
+        if len(second_clustering.labels_[second_clustering.labels_ == index]) > max_cluster_size:
+            max_cluster_size = len(second_clustering.labels_[second_clustering.labels_ == index])
+            max_cluster_index = index
+
+    third_embedding = second_embedding[[second_clustering.labels_ == max_cluster_index]]
+    print("third_embedding size", third_embedding.shape)
+    plt.cla()
+    fig, ax = plt.subplots(1, figsize=(14, 10))
+    plt.scatter(
+        third_embedding[:, 0],
+        third_embedding[:, 1],
+        s=3.0, cmap='Spectral', alpha=1.0)
+    plt.title('UMAP projection of the Anime dataset', fontsize=24)
+    plt.savefig('UMAP_DBSCAN_exclude_outlier.jpg')
+
+    #
+    image_path_list0 = image_path_list[clustering.labels_ == 0]
+    image_path_list1 = image_path_list[clustering.labels_ == 1]
+    image_path_list0_exclude_outlier = image_path_list0[[second_clustering.labels_ == max_cluster_index]]
+    embedding0 = second_embedding[[second_clustering.labels_ == max_cluster_index]]
+    embedding1 = embedding[clustering.labels_ == 1]
+    label0 = np.zeros(embedding0.shape[0])
+    label1 = np.ones(embedding1.shape[0])
+
+    final_embedding = np.concatenate([embedding0, embedding1])
+    final_labels = np.concatenate([label0, label1])
+    final_image_path_list = np.concatenate([image_path_list0_exclude_outlier, image_path_list1])
+
+    print('image_path_list0_exclude_outlier:', image_path_list0_exclude_outlier.shape)
+    print('image_path_list1:', image_path_list1.shape)
+    print('embedding0:', embedding0.shape)
+    print('embedding1:', embedding1.shape)
+    print('final_image_path_list:', final_image_path_list.shape)
+
+    plt.cla()
+    fig, ax = plt.subplots(1, figsize=(14, 10))
+    plt.scatter(
+        final_embedding[:, 0],
+        final_embedding[:, 1],
+        c=list(final_labels),
+        s=3.0, cmap='Spectral', alpha=1.0)
+    plt.setp(ax, xticks=[], yticks=[])
+    cbar = plt.colorbar(boundaries=np.arange(class_number + 1) - 0.5)
+    cbar.set_ticks(np.arange(class_number))
+    plt.title('UMAP projection of the Anime dataset', fontsize=24)
+    plt.savefig('UMAP_final_cluster.jpg')
 
     labe_list = np.arange(class_number)
     for label in labe_list:
         os.makedirs(os.path.join(data_dir, 'cluster_labels', str(label), 'style'), exist_ok=True)
         os.makedirs(os.path.join(data_dir, 'cluster_labels', str(label), 'smooth'), exist_ok=True)
 
-    for index, image_path in enumerate(image_path_list):
-        label = kmeans.labels_[index]
+    for index, image_path in enumerate(final_image_path_list):
+        label = int(final_labels[index])
         image_file = image_path.split('/')[-1]
         smooth_image_path = image_path
         style_image_path = image_path.replace('smooth', 'style')
@@ -99,6 +190,13 @@ def main():
         new_style_image_path = os.path.join(data_dir, 'cluster_labels', str(label), 'style', image_file)
         copyfile(smooth_image_path, new_smooth_image_path)
         copyfile(style_image_path, new_style_image_path)
+
+
+def main():
+    data_dir = '../style_dataset'
+    extract_feature(data_dir)
+    UMAP_embedding()
+    cluster(data_dir)
 
 
 if __name__ == '__main__':
